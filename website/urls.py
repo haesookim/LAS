73619d96958939b53a9adaf0c2784341875416bd
Heather Kim <haesoo1108@gmail.com>
2018-07-27 15:18:19 +0900

    Edited urls, designed top menu bar css

--- website/urls.py
+++ website/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^$', views.ideals, name = 'ideals'),
-    url(r'^$', views.members, name='members'),
-    url(r'^$', views.projects, name='projects'),
-    url(r'^$', views.presentations, name='presentations'),
+    url(r'', views.index),
+    url(r'^index/', views.index),
+    url(r'^ideals/', views.ideals),
+    url(r'^members/', views.members),
+    url(r'^projects/', views.projects),
+    url(r'^presentations/', views.presentations),
     #may or may not add 'history' page
 ]
